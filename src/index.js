a6e96beecff33f7053536f0a005ffada27e92292
lynxrenb <lynxrenb@gmail.com>
2021-02-17 20:21:27 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+ReactDom.render(
+    <React.StrictMode>
+        <h1>Saurav Jha</h1>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
