bae702ba1a047e713db5d8242e59a29f31568c10
lynxrenb <lynxrenb@gmail.com>
2021-02-17 20:50:47 +0530

    add toggle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './stylesheets/app.scss';
 
+import Toggle from './components/Toggle';
 import Particles from './components/Particles';
 
 export default function App() {
     return (
         <div className='app'>
+            <Toggle />
             <Particles />
         </div>
     )
