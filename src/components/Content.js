1dfbd79dfed11e934a3b406b42152d276f83fa1f
lynxrenb <lynxrenb@gmail.com>
2021-02-17 23:49:10 +0530

    add content component

---
+++ src/components/Content.js
@@ -0,0 +1,10 @@
+import '../stylesheets/content.scss'
+
+export default function Content() {
+    return (
+        <div className='container'>
+            <h1 className='name' aria-label='My name is Saurav Jha'>Saurav Jha</h1>
+            <h2 className='title' aria-label='I am a software engineer'>Software Engineer</h2>
+        </div>
+    )
+}
