ab3b33245d24c28f2392cfbfd6a7707b97d5460e
lynxrenb <lynxrenb@gmail.com>
2021-02-17 20:33:17 +0530

    add particles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './stylesheets/app.scss';
 
+import Particles from './components/Particles';
+
 export default function App() {
     return (
         <div className='app'>
+            <Particles />
         </div>
     )
 }
