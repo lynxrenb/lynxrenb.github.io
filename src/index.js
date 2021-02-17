468cd4a4d34e384dd564ebaefd727cbae2729379
lynxrenb <lynxrenb@gmail.com>
2021-02-17 20:36:03 +0530

    add App

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import App from './App';
 
 ReactDom.render(
     <React.StrictMode>
-        <h1>Saurav Jha</h1>
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 )
