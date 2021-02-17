7ef43583c7ed8cbe0a878f194d9ab169033441d1
lynxrenb <lynxrenb@gmail.com>
2021-02-17 23:52:16 +0530

    add content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './stylesheets/app.scss';
 
 import Toggle from './components/Toggle';
+import Content from './components/Content';
 import Particles from './components/Particles';
 
 export default function App() {
     return (
         <div className='app'>
             <Toggle />
+            <Content />
             <Particles />
         </div>
     )
